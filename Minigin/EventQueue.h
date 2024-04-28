6c76cbfdb3af86a51ded5b56369cb004c9d9037e
Dylan Burgisser <33450259+DijiOfficial@users.noreply.github.com>
2024-04-28 19:36:28 +0200

    added Alternating sound options

--- Minigin/EventQueue.h
+++ Minigin/EventQueue.h
@@ -15,10 +15,14 @@ namespace diji
         void Stop() { m_IsRunning = false; condition_.notify_one(); }
         void Start() { m_IsRunning = true; }
 
+        //originaly in PickUpManager but PickUp doesnt have access
+        void SwitchPickUpState() { m_FirstPickUp = !m_FirstPickUp; };
+        bool IsFirstPickUp() const { return m_FirstPickUp; };
     private:
         std::queue<std::pair<SoundId, int>> m_SoundQueue;
         std::mutex soundMutex_;
         bool m_IsRunning = false;
+        bool m_FirstPickUp = true;
 
         // Allows threads to synchronize their execution based on certain conditions. 
         // It provides a way for one thread to notify other threads when a particular condition becomes true, allowing them to proceed with their execution
