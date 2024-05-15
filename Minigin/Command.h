af8ef3fa176dcf220ca0b46580b5f4610339fe86
Dylan Burgisser <33450259+DijiOfficial@users.noreply.github.com>
2024-05-15 16:40:42 +0200

    changed Movement order for easier math

--- Minigin/Command.h
+++ Minigin/Command.h
@@ -6,11 +6,11 @@ namespace diji
 {
 	enum class Movement
 	{
-		Idle = 0,
-		Up = 1,
-		Down = 2,
-		Left = 3,
-		Right = 4,
+		Idle = -1,
+		Right = 0,
+		Down = 1,
+		Left = 2,
+		Up = 3,
 
 		TotalMovements
 	};
